2ab1363103e39bcf4dbb4f34f9fc287cce6d50a4
phrolow <sheludcko.bogdan@gmail.com>
2023-01-27 02:42:30 +0300

    merge asm

--- Compiler/compiler_main.cpp
+++ Compiler/compiler_main.cpp
@@ -1,30 +1,17 @@
 #include "compiler.h"
 
-int main(int argc, char **argv) {
-    if(argc < 2) {
-        printf("Too few arguments!");
-        exit(1);
-    }
-
-    if(!checkfile(argv[1])) {
-        printf("Invalid input file");
-        exit(2);
-    }
-
-    if(!checkfile(argv[2])) {
-        printf("Invalid output file");
-        exit(2);
-    }
+int language_compile(const char *in, const char *out) {
+    assert(in && out);
 
     tree *expression;
 
-    text txt = textFromFile(argv[1]);
+    text txt = textFromFile(in);
 
     expression = ReadExpression(txt.content);
 
     TreeDump(expression);
 
-    PrintAsm(expression, argv[2]);
+    PrintAsm(expression, out);
 
     TreeDtor(expression);
 
